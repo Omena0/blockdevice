ec47f5c8b38de04aba8d44ce37dce4a8befae338
Omena0 <omena0mc@gmail.com>
2025-09-25 09:59:42 +0000

    1F - Populate data frfr

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
+from scripts.get_version import get_blockdevice_version
 from setuptools import setup, Extension
 import pybind11
-import os
 
 # Define the extension module
 blockdevice_module = Extension(
@@ -19,18 +19,17 @@ blockdevice_module = Extension(
 
 setup(
     name='blockdevice',
-    version='0.1.0',
-    author='Your Name',
-    author_email='your.email@example.com',
+    version=get_blockdevice_version(),
+    author='Omena0',
+    author_email='omena0mc@gmail.com',
     description='A simple C++ Python library for block device operations',
-    long_description=open('README.md').read() if os.path.exists('README.md') else '',
+    long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    license='MIT',
     license_files=['COPYING.md'],
     url='https://github.com/Omena0/blockdevice',
     ext_modules=[blockdevice_module],
     packages=['blockdevice'],
-    python_requires='>=3.6',
+    python_requires='>=3.13',
     install_requires=[
         'pybind11>=2.6.0',
     ],
@@ -42,15 +41,7 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.12',
         'Programming Language :: Python :: 3.13',
-        'Operating System :: OS Independent',
         'Topic :: System :: Filesystems',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
